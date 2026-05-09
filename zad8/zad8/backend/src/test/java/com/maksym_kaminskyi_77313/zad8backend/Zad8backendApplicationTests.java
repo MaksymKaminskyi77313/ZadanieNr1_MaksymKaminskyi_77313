e6134d52caa104b5e354e7ee5901bb31a12277e0
MaksymKaminskyi77313 <mkamin20@stu.vistula.edu.pl>
2026-05-10 00:57:27 +0200

    77313 zad8: firebase backend form

---
+++ zad8/zad8/backend/src/test/java/com/maksym_kaminskyi_77313/zad8backend/Zad8backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksym_kaminskyi_77313.zad8backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad8backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
